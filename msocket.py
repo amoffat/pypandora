5df5839cd42dbb15a7c7c12766abfed736781885
Andrew Moffat <andrew.robert.moffat@gmail.com>
2011-10-13 22:05:40 -0500

    msocket upgrades

--- msocket.py
+++ msocket.py
@@ -6,10 +6,22 @@ import errno
 class MagicSocket(object):
     def __init__(self, sock):
         self.sock = sock
-        self._read = []
+        self.tmp_buffer = ""
+        self._read_gen = None
         
     def read_until(self, *delims, **kwargs):
+        if not self._read_gen: self._read_gen = self._read_until(*delims, **kwargs)
+        ret =  self._read_gen.next()
+        if ret: self._read_gen = None
+        return ret
+        
+    def _read_until(self, *delims, **kwargs):
         buf = kwargs.get("buf", 1024)
+        
+        num_bytes = 0
+        if len(delims) == 1 and isinstance(delims[0], int):
+            num_bytes = delims[0]
+            
         read = ""
         cursor = 0
         last_cursor = None
@@ -19,31 +31,48 @@ class MagicSocket(object):
         delims.reverse()
         current_delim = delims.pop()
         
+        def recv(buf):
+            read = ""
+            if self.tmp_buffer:
+                read += self.tmp_buffer[:buf]
+                self.tmp_buffer = self.tmp_buffer[buf:]
+                buf -= len(read)
+                if not buf: return read                
+            return read + self.sock.recv(buf)
+        
         while True:
-            # search through the data we have for the delimiters
-            lread = len(read)
-            while cursor < lread and cursor != last_cursor:
-                found = read.find(current_delim, cursor)
-                last_cursor = cursor
-                if found == -1:
-                    cursor = lread - len(current_delim)
-                else:
-                    if first_find is None: first_find = found
-                    cursor = found + len(current_delim)
-                    try: current_delim = delims.pop()
-                    except IndexError:
-                        if first_find == found: first_find = 0
-                        yield read[first_find:found]  
+            if not num_bytes:
+                # search through the data we have for the delimiters
+                lread = len(read)
+                while cursor < lread and cursor != last_cursor:
+                    found = read.find(current_delim, cursor)
+                    last_cursor = cursor
+                    if found == -1:
+                        cursor = lread - len(current_delim)
+                    else:
+                        if first_find is None: first_find = found
+                        cursor = found + len(current_delim)
+                        try: current_delim = delims.pop()
+                        except IndexError:
+                            if first_find == found: first_find = 0
+                            
+                            self.tmp_buffer = read[found:]
+                            yield read[first_find:found]
              
-            last_cursor = None
+                last_cursor = None
             
-            try: data = self.sock.recv(buf)
+            
+            try: data = recv(buf)
             except socket.error, err:
                 if err.errno is errno.EWOULDBLOCK:
                     yield None
             else:
                 if not data: yield False
                 read += data
+                if num_bytes and len(read) >= num_bytes:
+                    self.tmp_buffer = read[num_bytes:]
+                    yield read[:num_bytes]
+                
                 
                 
 host = "python.org"
@@ -52,5 +81,10 @@ sock.connect((host, 80))
 sock.send("GET / HTTP/1.1\r\nHost: %s\r\n\r\n" % host)
 
 msock = MagicSocket(sock)
-gen = msock.read_until("\r\n\r\n")
-print gen.next()     
+headers = msock.read_until("\r\n\r\n")
+headers = headers.split("\r\n")
+
+headers = dict([h.split(": ") for h in headers[1:]])
+
+length = int(headers["Content-Length"])
+print msock.read_until(length)
